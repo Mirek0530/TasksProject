785d9e206bc72d6b0e9e6e9d44b89ffe6f5679e2
Michal Miros <michalmiros7@wp.pl>
2023-04-25 18:13:30 +0200

    32.3 Daily mail template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -18,6 +21,11 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://mirek0530.github.io");
@@ -26,6 +34,29 @@ public class MailCreatorService {
         context.setVariable("company_name", adminConfig.getCompanyName());
         context.setVariable("goodbye_message", "Best regards");
         context.setVariable("preview", "New task info");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String sendDailyTasksInfoMail(String message) {
+        List<String> tasks = new ArrayList<>();
+        tasks.add("Prepare new template");
+        tasks.add("Refactor your code");
+        tasks.add("Create mail sending service");
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://mirek0530.github.io");
+        context.setVariable("button", "Manage tasks");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("goodbye_message", "Best regards");
+        context.setVariable("preview", "Daily task info");
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("tasks", tasks);
+        return templateEngine.process("mail/daily-tasks-mail", context);
     }
 }
